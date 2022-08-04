0249e16a07dc3dbcaea65338880d6343d40bf9c1
Paulo G Reis <paulosantos--@live.com>
2022-08-04 17:18:52 -0400

    Cria estrutura HTML do componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Header } from './components/Header';
-import { Post } from './Post';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import './global.css';
